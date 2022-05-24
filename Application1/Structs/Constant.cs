31ff7d0d11a4ae08d4387860a1242771c26e9e40
mothermetabot <sergiool.30@gmail.com>
2022-05-24 23:53:56 +0200

    Change service uri depending on Debug/Release build

--- Application1/Structs/Constant.cs
+++ Application1/Structs/Constant.cs
@@ -2,6 +2,10 @@
 {
     public readonly struct Constant
     {
+#if DEBUG
+        public const string REAL_TIME_SERVICE_URI = "http://localhost:5229/main";
+#else
         public const string REAL_TIME_SERVICE_URI = "http://localhost:5000/main";
+#endif
     }
 }
